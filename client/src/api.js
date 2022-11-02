c0f0c41da9054b763f7394f681bdc9e58352ae55
Chris Seaman <christopherseaman1983@gmail.com>
2022-11-02 11:09:26 +0000

    updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 const LIVE_API_URL = "https://cws-book-demo-api.netlify.app/.netlify/functions/api";
-const DEV_API_URL = "http://localhost:8080";
+const DEV_API_URL = "http://localhost:9000/.netlify/functions/api";
 
 export const API_URL = LIVE_API_URL;
